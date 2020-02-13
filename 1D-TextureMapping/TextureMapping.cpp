69562329c0089a88f51ab361f8d0fd29f3c6e35a
ainesh1998 <ainesh1998@outlook.com>
2020-02-13 17:04:05 +0000

    finished lab 2

--- 1D-TextureMapping/TextureMapping.cpp
+++ 1D-TextureMapping/TextureMapping.cpp
@@ -1,15 +1,11 @@
 #include <ModelTriangle.h>
 #include <CanvasTriangle.h>
 #include <DrawingWindow.h>
-// #include <CanvasPoint.h>
-// #include <Colour.h>
 #include <Utils.h>
 #include <glm/glm.hpp>
 #include <fstream>
 #include <vector>
 
-// using namespace std;
-// using namespace glm;
 
 #define WIDTH 800
 #define HEIGHT 640
@@ -31,6 +27,22 @@ void colourScale();
 
 DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 
+int main(int argc, char* argv[])
+{
+
+  SDL_Event event;
+
+  while(true)
+  {
+
+    // We MUST poll for events - otherwise the window will freeze !
+    if(window.pollForInputEvents(&event)) handleEvent(event);
+    update();
+    // Need to render the frame at the end, or nothing actually gets shown on the screen !
+    window.renderFrame();
+  }
+}
+
 std::vector<float> interpolate(float start, float end, int noOfValues){
   std::vector<float> vals;
   float stepVal = (end - start)/(noOfValues - 1);
@@ -39,9 +51,6 @@ std::vector<float> interpolate(float start, float end, int noOfValues){
     float temp = vals[i] + stepVal;
     vals.push_back(temp);
   }
-  // for(int i = 0; i < noOfValues; i++){
-  //   std::cout << vals[i] << ' ';
-  // }
   return vals;
 }
 
@@ -62,21 +71,6 @@ std::vector<glm::vec3> interpolate3(glm::vec3 start, glm::vec3 end, int noOfValu
     return vals;
 }
 
-int main(int argc, char* argv[])
-{
-
-  SDL_Event event;
-
-  while(true)
-  {
-
-    // We MUST poll for events - otherwise the window will freeze !
-    if(window.pollForInputEvents(&event)) handleEvent(event);
-    update();
-    // Need to render the frame at the end, or nothing actually gets shown on the screen !
-    window.renderFrame();
-  }
-}
 
 void drawTriangle(CanvasTriangle triangle){
   Colour c = triangle.colour;
@@ -86,34 +80,21 @@ void drawTriangle(CanvasTriangle triangle){
 }
 void drawTexturedTriangle(CanvasTriangle triangle,CanvasTriangle texture,std::vector<Colour> payload,int width,int height){
     //sort vertices in order (y position)
+
     if(triangle.vertices[1].y < triangle.vertices[0].y){
         std::swap(triangle.vertices[0],triangle.vertices[1]);
+        std::swap(texture.vertices[0], texture.vertices[1]);
     }
 
     if(triangle.vertices[2].y < triangle.vertices[1].y){
         std::swap(triangle.vertices[1],triangle.vertices[2]);
+        std::swap(texture.vertices[1],texture.vertices[2]);
         if(triangle.vertices[1].y < triangle.vertices[0].y){
             std::swap(triangle.vertices[1],triangle.vertices[0]);
-        }
-    }
-
-
-    if(texture.vertices[1].y < texture.vertices[0].y){
-        std::swap(texture.vertices[0],texture.vertices[1]);
-    }
-
-    if(texture.vertices[2].y < texture.vertices[1].y){
-        std::swap(texture.vertices[1],texture.vertices[2]);
-        if(texture.vertices[1].y < texture.vertices[0].y){
             std::swap(texture.vertices[1],texture.vertices[0]);
         }
     }
-    // std::cout << triangle << '\n';
-    // std::vector<float> vals = interpolate(2.2,8.5,3);
-    // for(int i = 0; i < 3; i++){
-    //   std::cout << vals[i] << ' ';
-    // }
-    // std::cout << '\n';
+
     CanvasPoint v1 = triangle.vertices[0];
     CanvasPoint v2 = triangle.vertices[1];
     CanvasPoint v3 = triangle.vertices[2];
@@ -124,17 +105,15 @@ void drawTexturedTriangle(CanvasTriangle triangle,CanvasTriangle texture,std::ve
     CanvasPoint u1 = texture.vertices[0];
     CanvasPoint u2 = texture.vertices[1];
     CanvasPoint u3 = texture.vertices[2];
-    // drawLine(v1,u1,Colour(0,255,0));
-    // drawLine(v2,u2,Colour(0,255,0));
-    // drawLine(v3,u3,Colour(0,255,0));
+
     float k_x = (u3.x-u1.x)/(v3.x-v1.x);
     float k_y = (u3.y-u1.y)/(v3.y-v1.y);
     int u4_x = u1.x + k_x * (v4.x-v1.x);
     int u4_y = u1.y + k_y * (v4.y-v1.y);
     CanvasPoint u4 = CanvasPoint(u4_x,u4_y);
-    drawLine(u4,u2,Colour(0,255,0));
-    drawLine(v4,v2,Colour(0,255,0));
-    // float d1 = ()/
+    // drawLine(u4,u2,Colour(0,255,0));
+    // drawLine(v4,v2,Colour(0,255,0));
+
     //Compute  flat bottom triangle
     float dxdyl = (v2.x - v1.x)/(v2.y -v1.y);
     float dux_dyl = (u2.x - u1.x)/(v2.y -v1.y);
@@ -159,9 +138,7 @@ void drawTexturedTriangle(CanvasTriangle triangle,CanvasTriangle texture,std::ve
         float du = (u_xr - u_xl)/dx;
         float dv = (u_yr - u_yl)/dx;
         for(int x = xl; x <= xr;x++){
-            ui = std::floor(ui);
-            vi = std::floor(vi);
-            Colour c = payload[ui + vi * width];
+            Colour c = payload[(int) ui + (int) vi * width];
             window.setPixelColour(x,y,c.packed_colour());
             ui += du;
             vi += dv;
@@ -191,26 +168,16 @@ void drawTexturedTriangle(CanvasTriangle triangle,CanvasTriangle texture,std::ve
   xr = v3.x;
   u_xr = u3.x;
   u_yr = u3.y;
-  // std::cout <<  << '\n';
+
   for (int y = v3.y; y > v2.y; y--){
       float ui = u_xl;
       float vi = u_yl;
       float dx = xr - xl;
       float du = (u_xr - u_xl)/dx;
       float dv = (u_yr - u_yl)/dx;
-      // std::cout << xl << '\n';
-      // std::cout << xr << '\n';
-      // std::cout << "" << '\n';
+
       for(int x = xl; x <= xr;x++){
-          ui = std::floor(ui);
-          vi = std::floor(vi);
-          std::cout << ui << '\n';
-          std::cout << vi << '\n';
-          std::cout << "" << '\n';
-          Colour c = payload[ui + vi * width];
-          // std::cout << x << '\n';
-          // std::cout << y << '\n';
-          // std::cout << "" << '\n';
+          Colour c = payload[(int) ui + (int) vi * width];
          window.setPixelColour(x,y,c.packed_colour());
           ui += du;
           vi += dv;
@@ -221,7 +188,7 @@ void drawTexturedTriangle(CanvasTriangle triangle,CanvasTriangle texture,std::ve
       xr -= dxdyr;
       u_xr -= dux_dyr;
       u_yr -= duy_dyr;
-}
+  }
 
 
 
@@ -238,20 +205,14 @@ void drawFilledTriangle(CanvasTriangle triangle){
             std::swap(triangle.vertices[1],triangle.vertices[0]);
         }
     }
-    // std::cout << triangle << '\n';
-    // std::vector<float> vals = interpolate(2.2,8.5,3);
-    // for(int i = 0; i < 3; i++){
-    //   std::cout << vals[i] << ' ';
-    // }
-    // std::cout << '\n';
+
     CanvasPoint v1 = triangle.vertices[0];
     CanvasPoint v2 = triangle.vertices[1];
     CanvasPoint v3 = triangle.vertices[2];
     float slope = (v2.y - v1.y)/(v3.y - v1.y);
     int newX = v1.x + slope * (v3.x - v1.x);
     CanvasPoint v4 = CanvasPoint(newX,v2.y);
-    // std::cout << triangle << '\n';
-    // std::cout << v4 << '\n';
+
     Colour c = triangle.colour;
     drawLine(v2,v4,Colour(255,255,255));
 
@@ -393,8 +354,7 @@ void handleEvent(SDL_Event event)
         int width;
         int height;
         std::vector<Colour> payload = readPPM("texture.ppm",&width,&height);
-        // std::cout << width << '\n';
-        // std::cout << height << '\n';
+
         CanvasTriangle triangle = CanvasTriangle(CanvasPoint(160,10),
                                   CanvasPoint(300,230),
                                 CanvasPoint(10,150),Colour(255,255,255));
@@ -403,8 +363,8 @@ void handleEvent(SDL_Event event)
                                   CanvasPoint(395,380),
                                 CanvasPoint(65,330),Colour(0,0,255));
         // displayPicture(v,width,height);
-        drawTriangle(texture);
-        drawTriangle(triangle);
+        // drawTriangle(texture);
+        // drawTriangle(triangle);
         drawTexturedTriangle(triangle,texture,payload,width,height);
     }
     else if(event.key.keysym.sym == SDLK_c){
@@ -427,7 +387,7 @@ std::vector<Colour> readPPM(char* filename,int* width, int* height){
     stream.open(filename,std::ifstream::in);
     char encoding[3];
     stream.getline(encoding,3);
-    // std::cout << encoding << '\n';
+
     char comment[256];
     stream.getline(comment,256);
     char widthText[256];

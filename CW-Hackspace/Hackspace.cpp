547c6a61387909b855e3d0e39ab6ed15043bc809
ainesh1998 <ainesh1998@outlook.com>
2020-04-25 11:05:33 +0800

    seems to fully work now

--- CW-Hackspace/Hackspace.cpp
+++ CW-Hackspace/Hackspace.cpp
@@ -1926,76 +1926,81 @@ std::vector<ModelTriangle> removeOutsideTriangles(std::vector<ModelTriangle> tri
 }
 
 std::vector<CanvasTriangle> fragmentTriangle(CanvasTriangle triangle) {
-    std::vector<CanvasTriangle> clippedTriangles = {};
-    // std::vector<glm::vec2> rangeChecks = {glm::vec2()};
-    // std::vector<int> rangeVals = {50, 50, WIDTH-51, HEIGHT-51};
+    std::vector<CanvasTriangle> clippedTriangles = {triangle};
 
-    for (int i = 3; i < 4; i++) {
-        CanvasTriangle orderY = triangle;
-        if (i%2 == 0) swapTriangleXY(&orderY);
-        // mirror triangle along x axis - y = (HEIGHT-1)-y
-        int mirrorSize = i == 2 ? WIDTH : HEIGHT;
-        if (i >= 2) mirrorTriangleX(&orderY, mirrorSize);
 
-        order_triangle(&orderY);
-        int pointInsideCount = 0;
+    for (int i = 0; i < 4; i++) {
+        std::vector<CanvasTriangle> newClippedTriangles;
 
-        for (int j = 0; j < 3; j++) {
-            if (orderY.vertices[j].y >= 50) pointInsideCount++;
-            // if (i <= 1 && triangle.vertices[j].y >= rangeVals[i]) pointInsideCount++;
-            // else if (i >= 2 && triangle.vertices[j].y <= rangeVals[i]) pointInsideCount++;
-        }
+        for (int j = 0; j < clippedTriangles.size(); j++) {
+            CanvasTriangle orderY = clippedTriangles[j];
 
-        if (pointInsideCount == 3) clippedTriangles.push_back(triangle);
+            if (i%2 == 0) swapTriangleXY(&orderY);
+            // mirror triangle along x axis - y = (HEIGHT-1)-y
+            int mirrorSize = i == 2 ? WIDTH : HEIGHT;
+            if (i >= 2) mirrorTriangleX(&orderY, mirrorSize);
 
-        else if (pointInsideCount == 2) {
-            CanvasPoint v0 = orderY.vertices[0];
-            CanvasPoint v1 = orderY.vertices[1];
-            CanvasPoint v2 = orderY.vertices[2];
+            order_triangle(&orderY);
+            int pointInsideCount = 0;
 
-            int intersection_x1 = v0.x + (50-v0.y)*(v1.x-v0.x)/(v1.y-v0.y);
-            double intersection_z1 = v0.depth + (50-v0.y)*(v1.depth-v0.depth)/(v1.y-v0.y);
-            int intersection_x2 = v0.x + (50-v0.y)*(v2.x-v0.x)/(v2.y-v0.y);
-            double intersection_z2 = v0.depth + (50-v0.y)*(v2.depth-v0.depth)/(v2.y-v0.y);
-
-            CanvasTriangle t1 = orderY;
-            t1.vertices[0].x = intersection_x2; t1.vertices[0].y = 50; t1.vertices[0].depth = intersection_z2;
-            CanvasTriangle t2 = orderY;
-            t2.vertices[0].x = intersection_x1; t2.vertices[0].y = 50; t2.vertices[0].depth = intersection_z1;
-            t2.vertices[2].x = intersection_x2; t2.vertices[2].y = 50; t2.vertices[2].depth = intersection_z2;
-
-            if (i >= 2) {
-                mirrorTriangleX(&t1, mirrorSize);
-                mirrorTriangleX(&t2, mirrorSize);
+            for (int k = 0; k < 3; k++) {
+                if (orderY.vertices[k].y >= 50) pointInsideCount++;
             }
 
-            if (i%2 == 0) {
-                swapTriangleXY(&t1);
-                swapTriangleXY(&t2);
+            if (pointInsideCount == 3) newClippedTriangles.push_back(clippedTriangles[j]);
+
+            else if (pointInsideCount == 2) {
+                CanvasPoint v0 = orderY.vertices[0];
+                CanvasPoint v1 = orderY.vertices[1];
+                CanvasPoint v2 = orderY.vertices[2];
+
+                int intersection_x1 = v0.x + (50-v0.y)*(v1.x-v0.x)/(v1.y-v0.y);
+                double intersection_z1 = v0.depth + (50-v0.y)*(v1.depth-v0.depth)/(v1.y-v0.y);
+                int intersection_x2 = v0.x + (50-v0.y)*(v2.x-v0.x)/(v2.y-v0.y);
+                double intersection_z2 = v0.depth + (50-v0.y)*(v2.depth-v0.depth)/(v2.y-v0.y);
+
+                CanvasTriangle t1 = orderY;
+                t1.vertices[0].x = intersection_x2; t1.vertices[0].y = 50; t1.vertices[0].depth = intersection_z2;
+                CanvasTriangle t2 = orderY;
+                t2.vertices[0].x = intersection_x1; t2.vertices[0].y = 50; t2.vertices[0].depth = intersection_z1;
+                t2.vertices[2].x = intersection_x2; t2.vertices[2].y = 50; t2.vertices[2].depth = intersection_z2;
+
+                if (i >= 2) {
+                    mirrorTriangleX(&t1, mirrorSize);
+                    mirrorTriangleX(&t2, mirrorSize);
+                }
+
+                if (i%2 == 0) {
+                    swapTriangleXY(&t1);
+                    swapTriangleXY(&t2);
+                }
+
+                newClippedTriangles.push_back(t1);
+                newClippedTriangles.push_back(t2);
             }
 
-            clippedTriangles.push_back(t1);
-            clippedTriangles.push_back(t2);
+            else if (pointInsideCount == 1) {
+                CanvasPoint v0 = orderY.vertices[2];
+                CanvasPoint v1 = orderY.vertices[1];
+                CanvasPoint v2 = orderY.vertices[0];
+
+                int intersection_x1 = v0.x + (50-v0.y)*(v1.x-v0.x)/(v1.y-v0.y);
+                double intersection_z1 = v0.depth + (50-v0.y)*(v1.depth-v0.depth)/(v1.y-v0.y);
+                int intersection_x2 = v0.x + (50-v0.y)*(v2.x-v0.x)/(v2.y-v0.y);
+                double intersection_z2 = v0.depth + (50-v0.y)*(v2.depth-v0.depth)/(v2.y-v0.y);
+
+                CanvasTriangle t1 = orderY;
+                t1.vertices[0].x = intersection_x1; t1.vertices[0].y = 50; t1.vertices[0].depth = intersection_z1;
+                t1.vertices[1].x = intersection_x2; t1.vertices[1].y = 50; t1.vertices[1].depth = intersection_z2;
+
+                if (i >= 2) mirrorTriangleX(&t1, mirrorSize);
+                if (i%2 == 0) swapTriangleXY(&t1);
+                newClippedTriangles.push_back(t1);
+            }
         }
 
-        else if (pointInsideCount == 1) {
-            CanvasPoint v0 = orderY.vertices[2];
-            CanvasPoint v1 = orderY.vertices[1];
-            CanvasPoint v2 = orderY.vertices[0];
-
-            int intersection_x1 = v0.x + (50-v0.y)*(v1.x-v0.x)/(v1.y-v0.y);
-            double intersection_z1 = v0.depth + (50-v0.y)*(v1.depth-v0.depth)/(v1.y-v0.y);
-            int intersection_x2 = v0.x + (50-v0.y)*(v2.x-v0.x)/(v2.y-v0.y);
-            double intersection_z2 = v0.depth + (50-v0.y)*(v2.depth-v0.depth)/(v2.y-v0.y);
-
-            CanvasTriangle t1 = orderY;
-            t1.vertices[0].x = intersection_x1; t1.vertices[0].y = 50; t1.vertices[0].depth = intersection_z1;
-            t1.vertices[1].x = intersection_x2; t1.vertices[1].y = 50; t1.vertices[1].depth = intersection_z2;
-
-            if (i >= 2) mirrorTriangleX(&t1, mirrorSize);
-            if (i%2 == 0) swapTriangleXY(&t1);
-            clippedTriangles.push_back(t1);
-        }
+        // std::cout << newClippedTriangles.size() << '\n';
+        clippedTriangles = newClippedTriangles;
     }
 
     return clippedTriangles;
